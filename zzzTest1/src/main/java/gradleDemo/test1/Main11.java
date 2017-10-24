fe1bdb4145d6990bf043dadd77d04cacfd9fbcb0
ccho <ccho@smart-trade.net>
2017-10-24 15:23:44 +0200

    add reportTestF

--- zzzTest1/src/main/java/gradleDemo/test1/Main11.java
+++ zzzTest1/src/main/java/gradleDemo/test1/Main11.java
@@ -3,7 +3,8 @@ package gradleDemo.test1;
 public class Main11 {
 	public static void main(String[] args) {
 		System.out.println("Hello Gradle from zzzTest1");
-		
+		System.out.println("JAVA_OPTS: " + System.getenv("JAVA_OPTS"));
+		System.out.println(System.getenv("JAVA_OPTS"));
 		for (int i = 0; i < 1; i++) {
 			String[] s = new String[1000000];
 		}
