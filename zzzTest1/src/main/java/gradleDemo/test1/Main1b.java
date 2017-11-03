fcb6f726cd5b6d494c5972345e3ba5feee5830a2
ccho <ccho@smart-trade.net>
2017-11-03 14:50:46 +0100

    add plugin

--- zzzTest1/src/main/java/gradleDemo/test1/Main1b.java
+++ zzzTest1/src/main/java/gradleDemo/test1/Main1b.java
@@ -4,10 +4,6 @@ public class Main1b {
 	public static void main(String[] args) {
 		System.out.println("@Develop --");
 		System.out.println("Hello Gradle from zzzTest1");
-		System.out.println("JAVA_OPTS: " + System.getenv("JAVA_OPTS"));
-		for (int i = 0; i < 1; i++) {
-			String[] s = new String[1000000];
-		}
 		try {
 			Class.forName("gradleDemo.test1.Test1TestSuite");
 		} catch (ClassNotFoundException e) {
